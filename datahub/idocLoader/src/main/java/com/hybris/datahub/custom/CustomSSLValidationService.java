d907087e290318cbdd1164cec7a7eaa0bec0b189
Marcin Cieślak <cieslo@poczta.fm>
2018-10-24 10:29:50 +0200

    overridden check for https

---
+++ datahub/idocLoader/src/main/java/com/hybris/datahub/custom/CustomSSLValidationService.java
@@ -0,0 +1,12 @@
+package com.hybris.datahub.custom;
+
+import com.hybris.datahub.validation.DefaultSSLValidationService;
+
+public class CustomSSLValidationService extends DefaultSSLValidationService
+{
+	@Override
+	public void afterPropertiesSet() throws Exception
+	{
+		return;
+	}
+}
