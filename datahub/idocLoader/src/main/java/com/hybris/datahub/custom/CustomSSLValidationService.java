6f1c9117c75312f0d879a901f3fd73892a02bf58
Cieslo <41468689+Cieslo@users.noreply.github.com>
2018-10-25 09:05:30 +0200

    without extends

--- datahub/idocLoader/src/main/java/com/hybris/datahub/custom/CustomSSLValidationService.java
+++ datahub/idocLoader/src/main/java/com/hybris/datahub/custom/CustomSSLValidationService.java
@@ -1,12 +1,44 @@
 package com.hybris.datahub.custom;
 
-import com.hybris.datahub.validation.DefaultSSLValidationService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Required;
 
-public class CustomSSLValidationService extends DefaultSSLValidationService
+public class CustomSSLValidationService implements InitializingBean
 {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomSSLValidationService.class);
+	private boolean httpsEnabled;
+	private String datahubServerUrl;
+	
 	@Override
 	public void afterPropertiesSet() throws Exception
 	{
 		return;
+	}
+	
+	void logWarning()
+	{
+		LOGGER.warn("\n" +
+				"************************************************************************************\n" +
+				"* Data Hub is running in HTTP mode.                                                *\n" +
+				"*                                                                                  *\n" +
+				"* This may be suitable for DEVELOPMENT or in PRODUCTION                            *\n" +
+				"* when using a TLS termination proxy.                                              *\n" +
+				"* Require HTTPS protocol by setting property datahub.security.https.enabled=true   *\n" +
+				"************************************************************************************");
+	}
+
+	@Required
+	public void setHttpsEnabled(final boolean httpsEnabled)
+	{
+		this.httpsEnabled = httpsEnabled;
+	}
+
+	@Required
+	public void setDatahubServerUrl(final String datahubServerUrl)
+	{
+		this.datahubServerUrl = datahubServerUrl;
 	}
 }
